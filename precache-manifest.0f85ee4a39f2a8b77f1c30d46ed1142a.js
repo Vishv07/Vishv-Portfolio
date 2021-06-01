1804b3c096ccd6bba4234bbbda1f59784999acc6
Vishv Kakadiya <krishnakakadiya9@gmail.com>
2021-06-01 22:43:20 +0530

    Updates

---
+++ precache-manifest.0f85ee4a39f2a8b77f1c30d46ed1142a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd84cb9609b1ac94893d51c717e014f5",
+    "url": "/Vishv-Portfolio/index.html"
+  },
+  {
+    "revision": "a02a368358bd99669e1d",
+    "url": "/Vishv-Portfolio/static/css/main.05322e77.chunk.css"
+  },
+  {
+    "revision": "54d719273b369efc28da",
+    "url": "/Vishv-Portfolio/static/js/2.eae81a98.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Vishv-Portfolio/static/js/2.eae81a98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a02a368358bd99669e1d",
+    "url": "/Vishv-Portfolio/static/js/main.03d13313.chunk.js"
+  },
+  {
+    "revision": "f206ef5aff70fbfc50f5",
+    "url": "/Vishv-Portfolio/static/js/runtime-main.a742d3d1.js"
+  },
+  {
+    "revision": "d8c7179d29aec8bcf0f09304c882ba32",
+    "url": "/Vishv-Portfolio/static/media/listnr.d8c7179d.png"
+  },
+  {
+    "revision": "77d9312a554f7e61aa44ff063aeaf633",
+    "url": "/Vishv-Portfolio/static/media/profile.77d9312a.svg"
+  },
+  {
+    "revision": "d9cdef32af3c76e2b25699c7b8466a6e",
+    "url": "/Vishv-Portfolio/static/media/silverwing.d9cdef32.jpg"
+  }
+]);
